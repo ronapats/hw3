58c95513f8a42d63b66354799c42d0d1fda0f3be
ronapats <ronapat.srisupavanich@kellogg.northwestern.edu>
2023-02-16 02:36:56 +0000

    Final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
     def new
       @post = Post.new
-      @post.place_id = params["place_id"]
+      @post["place_id"] = params["place_id"]
     end
   
     def create
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
       @post["posted_on"] = params["post"]["posted_on"]
       @post["place_id"] = params["post"]["place_id"]
       @post.save
-      redirect_to "/places/#{@post.company_id}"
+      redirect_to "/places/#{@post["place_id"]}"
     end
   
   end
